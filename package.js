b2c5b221f24e1bcd67c2ad45eaa25dddb1d5e4df
HaNdTriX <handtrix@gmail.com>
2015-07-23 14:59:17 +0200

    fix typo

--- package.js
+++ package.js
@@ -15,6 +15,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('handtrix:chrome-extension-client');
+  api.use('handtrix:chrome-extension-utils');
   api.addFiles('meteor/client/chrome-extension-tests.js');
 });
